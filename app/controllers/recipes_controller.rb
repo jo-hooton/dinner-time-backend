c3567f93a2b192e2a4c1ca0bbc28d88a9f0402f3
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-06 19:11:04 +0000

    exclude param recipes endpoint

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,23 @@
 class RecipesController < ApplicationController
   def index
-    if params[:query].present?
-      @recipes = Recipe.search_by_ingredients(params[:query])
-    else
-      @recipes = Recipe.all
+    # pg search for recipes containing specified ingredients
+    @recipes = params[:query].present? ? Recipe.search_by_ingredients(params[:query]) : Recipe.all
+
+    # filter by prep + cook time if specified
+    if params[:time].present?
+      @recipes = @recipes.where("cook_minutes + prep_minutes <= ?", params[:time])
     end
 
-    @recipes = @recipes.where("cook_minutes + prep_minutes <= ?", params[:time]) if params[:time].present?
-    @recipes = @recipes.page(params[:page]).per(params[:per] || 9)
+    # exclude recipes containing specified ingredients
+    if params[:exclude].present?
+      exclude_ingredients = params[:exclude].split
+      exclude_ingredients.each do |ingredient|
+        @recipes = @recipes.where.not("ingredients::text ILIKE ?", "%#{ingredient}%")
+      end
+    end
+
+    # pagination
+    @recipes = @recipes.page(params[:page]).per(params[:per] || 10)
 
     render json: @recipes
   end
