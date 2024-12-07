b127ebf672586251a5ec1a45ca5187f344c050fb
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-07 13:05:53 +0000

    render config

---
+++ bin/render-build.sh
@@ -0,0 +1,10 @@
+# exit on error
+set -o errexit
+
+bundle install
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+bundle exec rails db:migrate
