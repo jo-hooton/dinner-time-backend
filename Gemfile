9131ac08c5b05d556b2d1e9cf6850639dcc9b98f
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-07 12:28:39 +0000

    env config

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
 
   gem 'rspec-rails'
   gem 'rack-cors', require: 'rack/cors'
+  gem 'dotenv'
 end
 
 
--- config/application.rb
+++ config/application.rb
@@ -29,11 +29,11 @@ module DinnerTimeBackend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins 'http://localhost:5173' # Frontend URL
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
-      end
-    end
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     origins ENV['FRONTEND_URL']
+    #     resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head]
+    #   end
+    # end
   end
 end
