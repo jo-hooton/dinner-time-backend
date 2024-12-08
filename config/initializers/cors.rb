a2e1c15ed682c6148e2cd6cf6f9dce361a1d2832
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2024-12-08 12:56:02 +0000

    FRONTEND_URL Default to localhost if ENV is not set

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONTEND_URL']
+    origins ENV['FRONTEND_URL'] || 'http://localhost:5173' # Default to localhost if ENV is not set
     resource '*',
       headers: :any,
       methods: %i[get post put patch delete options head]
